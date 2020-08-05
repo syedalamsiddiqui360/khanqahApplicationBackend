487e6cdbf3a1f9833a78a85ab961be4019f9d796
Hamza <imhamza@outlook.com>
2020-08-05 14:49:13 +0500

    checkout function get amount from cart

--- api/controllers/cart_controller.js
+++ api/controllers/cart_controller.js
@@ -198,7 +198,6 @@ exports.deleteCartItem = async (req, res, next) => {
 
 calculate_product_price = async (product_id, quantity) => {};
 
-
 /*****THIS FUNCTION IS USED TO UPDATE CART TABLE WHEN EVER CHANGES (INSERT UPDATE DELETE) IN ORDER TABLE ENTRIES. IT CALCULATE CART TOTAL BASE ON ORDER ITEMS.****/
 update_cart_calculations = async (cart_id) => {
   var cart_object = await cart.findOne({
@@ -249,60 +248,75 @@ update_cart_calculations = async (cart_id) => {
   }
 };
 
-exports.checkout = async ( req,res) => {
-
+exports.checkout = async (req, res) => {
   console.log("checkout called");
   const mollieClient = createMollieClient({
     apiKey: "test_bv74rGDe9wC22EcHdyw3d7C9BgQtRw",
   });
   console.log("checkout object created");
+
+  // cart id to get cart. And from cart object we will get order price.
+  //first validate if cart id exists
+  var cart_id = req.body.cart_id;
+  cart_response = await cart.findOne({ where: { id: cart_id } });
+  if (!cart_response) {
+    return res.status(422).json({
+      message: "Invalid Cart ID",
+    });
+  }
+
   mollieClient.payments
     .create({
       amount: {
         value: "10.00",
         currency: "EUR",
       },
-      description: 'Order #12345',
-      method:'creditcard',
-      redirectUrl: 'https://backend.develop.pdt.agifly.cloud/cart/receive-payment-response',
-      //webhookUrl: 'https://backend.develop.pdt.agifly.cloud/cart/receive-payment-response',
-      webhookUrl: 'https://pdt.requestcatcher.com/',
+      description: "Order #"+cart_id,
+      method: "creditcard",
+      redirectUrl:
+        "https://backend.develop.pdt.agifly.cloud/cart/receive-payment-response",
+      webhookUrl: 'https://backend.develop.pdt.agifly.cloud/cart/receive-payment-response',
+      //webhookUrl: "https://pdt.requestcatcher.com/",
       metadata: {
-        order_id: '12345',
+        order_id: "12345",
       },
     })
     .then((payment) => {
-      console.log("payment",payment);
+      console.log("payment", payment);
       res.status(200).json({
-        "payment_link":payment._links.checkout.href
+        payment_link: payment._links.checkout.href,
       });
     })
     .catch((error) => {
       console.log(error);
-      res.status(500).json(error);
+      res.status(422).json({
+        error: error,
+        message: "error in processing your payment.",
+      });
     });
 };
 
-
-
-exports.receivePaymentResponse = async (req,res) => {
+exports.receivePaymentResponse = async (req, res) => {
   console.log("getting payment details");
-  console.log("body",req.body);
+  console.log("body", req.body);
   res.status(200).json("msg");
-  // var transaction_id=req.body.id;
+  // var transaction_id = req.body.id;
   // const mollieClient = createMollieClient({
   //   apiKey: "test_bv74rGDe9wC22EcHdyw3d7C9BgQtRw",
   // });
-  
-  // mollieClient.payments.get(transaction_id)
+
+  // mollieClient.payments
+  //   .get(transaction_id)
   //   .then((payment) => {
-  //     console.log("transaction details",payment);
+  //     console.log("transaction details", payment);
   //     res.status(200).json(payment);
   //   })
   //   .catch((error) => {
   //     console.log(error);
-  //     res.status(500).json(error);
+  //     res.status(422).json({
+  //       error: error,
+  //       message:
+  //         "error in getting transaction details. Check your transaction id.",
+  //     });
   //   });
-
-  
 };
