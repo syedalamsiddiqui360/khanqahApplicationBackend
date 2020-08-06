e82866f0116d06b9d4c76098729273dd444977f5
Hamza Aslam <ha@agifly.be>
2020-08-06 07:20:36 +0000

    Menu links update cart update

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -14,12 +14,12 @@ router.post("/add_to_cart", cartRequests.addToCart,  CartController.addToCart);
 router.put("/update_cart_quantity", cartRequests.updateCartQuantity , CartController.updateCartQuantity);
 router.delete("/delete_cart_item", cartRequests.deleteCartItem,  CartController.deleteCartItem);
 router.get("/checkout",   CartController.checkout);
-router.all("/receive-payment-response", upload.none(),  CartController.receivePaymentResponse);
+router.all("/receive-payment-response",   CartController.receivePaymentResponse);
 
 
-// router.all('/profile', upload.none(), function (req, res, next) {
-//     res.send(req.body);
-//   })
+router.all('/profile', upload.none(), function (req, res, next) {
+    res.send(req.body);
+  })
 
 
 module.exports = router;
